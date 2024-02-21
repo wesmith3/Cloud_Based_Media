6c8568e9047b999df6406f6bcfb72e48fe33ce40
Wesley <wesmith314@gmail.com>
2024-02-21 09:42:58 -0800

    adds services/contact links

--- src/components/RouterPage.js
+++ src/components/RouterPage.js
@@ -1,21 +1,23 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import Projects from "./Projects";
-import AboutPage from "./AboutPage";
-import Portfolio from "./Portfolio";
+import { Routes, Route } from "react-router-dom"
+import Home from "./Home"
+import Projects from "./Projects"
+import AboutPage from "./AboutPage"
+import Contact from "./Contact"
+import Services from "./Services"
+import Portfolio from "./Portfolio"
 
 
 function RouterPage() {
-
   return (
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/projects" element={<Projects/>} />
-
+        <Route path="/services" element={<Services/>} />
+        <Route path="/contact" element={<Contact/>} />
       </Routes>
-  );
+  )
 }
 
-export default RouterPage;
+export default RouterPage
