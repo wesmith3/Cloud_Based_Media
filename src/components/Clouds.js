9a2c45c790f57997da86b18184c872a35ba00b25
Wesley <wesmith314@gmail.com>
2024-02-22 07:16:21 -0800

    adds cloud imports

--- src/components/Clouds.js
+++ src/components/Clouds.js
@@ -2,6 +2,7 @@ import cloud1 from './pics/cloud1.png'
 import cloud2 from './pics/cloud2.png'
 import cloud3 from './pics/cloud3.png'
 import cloud4 from './pics/cloud4.png'
+import cloud5 from './pics/cloud5.png'
 
 function Clouds() {
   return (
