f81d71f6d9ed8e7149776b45a320ee9b39deb4cc
Wesley <wesmith314@gmail.com>
2024-02-22 22:16:17 -0800

    adds Home/About Us link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from 'semantic-ui-react'
 
 function NavBar() {
   return (
     <div>
-      
+      <Link to='/'>
+        <Button className='menu-btn'>
+          Home
+        </Button>
+      </Link>
+      <Link to='/about'>
+        <Button className='menu-btn'>
+          About Us
+        </Button>
+      </Link>
     </div>
   )
 }
