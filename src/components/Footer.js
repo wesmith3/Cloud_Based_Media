d23243e296bf6a805c65c6ab31bec53803c30e45
Wesley <wesmith314@gmail.com>
2024-02-23 13:16:51 -0800

    adds three grid columns

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,39 @@
-import { Segment, Container, Grid, Header, List, Button } from "semantic-ui-react";
+import {
+  Segment,
+  Container,
+  Grid,
+  Header,
+  List,
+  Button,
+} from "semantic-ui-react";
 
 function Footer() {
   return (
     <div>
-        <Segment>
-            <Container>
-                <Grid>
-                    <Grid.Row>
-                        <Grid.Column>
-                            <Header>
-                                <List>
-                                    <List.Item>Sitemap</List.Item>
-                                    <List.Item>Contact Us</List.Item>
-                                    <List.Item>FAQ's</List.Item>
-                                </List>
-                            </Header>
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-            </Container>
-        </Segment>
-      
+      <Segment>
+        <Container>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column>
+                <Header />
+                  <List>
+                    <List.Item>Sitemap</List.Item>
+                    <List.Item>Contact Us</List.Item>
+                    <List.Item>FAQ's</List.Item>
+                  </List>
+              </Grid.Column>
+              <Grid.Column>
+                <Header />
+              </Grid.Column>
+              <Grid.Column>
+                
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </Segment>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
